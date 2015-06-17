c5c4b42ef2c2ab97b81ee1b4ab7a41d60c8b40db
Eric Minikel <ericminikel@gmail.com>
2015-06-17 11:59:42 -0400

    moved associate into its own function; added benchmarking

--- associate_fragments.r
+++ associate_fragments.r
@@ -43,4 +43,48 @@ for (j in 1:n_frags) {
 	association$p_val[j] = fisher_result$p.value
 }
 
-# will need to do some bootstrapping or something to decide how to handle the multiple testing burden
+
+associate_fragments = function(mat, is_hit) {
+  # initialize data frame of results to return
+  association = data.frame(fragment=colnames(mat), 
+                           n_compounds=integer(dim(mat)[2]), 
+                           n_hits=integer(dim(mat)[2]), 
+                           or=numeric(dim(mat)[2]), 
+                           p_val=numeric(dim(mat)[2]))
+  # test each fragment for association with hit status
+  for (j in 1:dim(mat)[2]) {
+    n_compounds = sum(mat[,j])
+    n_hits = sum(mat[,j] & is_hit)
+    ctable = ftable(hit=is_hit, frag=as.logical(mat[,j])) # * see comments below
+    fisher_result = fisher.test(ctable,alternative='two.sided')
+    association$n_compounds[j] = n_compounds
+    association$n_hits[j] = n_hits
+    association$or[j] = fisher_result$estimate
+    association$p_val[j] = fisher_result$p.value
+  }
+  return (association)
+}
+
+# * based on benchmarking, this seems to be the most computationally intensive step, at about .01 seconds per iteration.
+# we have 1977 columns and the whole function takes ~35s (elapsed) so this line is >50% of the compute time here.
+# it appears to be just slightly faster to use ftable than table, and to feed 'hit' and 'frag' in directly as vectors
+# rather than first wrapping them into a data.frame; but beyond that I am at a loss for how to make this faster.
+# to benchmark the whole function: require(rbenchmark); benchmark(associate_fragments(mat,is_hit),replications=1)
+# but unlike in Python, benchmark doesn't break it down by calls; you have to wrap each line in benchmark separately
+
+bootstrap = function(mat, n_hits, n_iter, fdr=c(.10,.05,.01)) {
+  p_val_distributions = {}
+  for (iter in 1:n_iter) {
+    hits = sample(1:dim(mat)[1],size=n_hits,replace=FALSE)
+    is_hit = 1:dim(mat)[1] %in% hits
+    associations = associate_fragments(mat, is_hit)
+    p_val_distributions[[iter]] = associations$p_val
+  }
+  fdr_cutoffs = quantile(unlist(p_val_distributions), fdr)
+  return_value = {}
+  return_value[['fdr_cutoffs']] = fdr_cutoffs
+  return_value[['p_val_distributions']] = p_val_distributions
+  return (return_value)
+}
+
+
